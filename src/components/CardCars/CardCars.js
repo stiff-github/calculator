bd2e2472a61e849fb3f4a55ca4d279881eafa42f
stiff-github <victory.adm@yandex.ru>
2020-02-12 22:13:47 +0300

    json

--- src/components/CardCars/CardCars.js
+++ src/components/CardCars/CardCars.js
@@ -5,7 +5,7 @@ export default function CardCars(props) {
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Body>
-        <Card.Text>MSRP: <b>${props.carsCard['msrp']}</b></Card.Text>
+        <Card.Text>MSRP: <b>${props.priceMSRP}</b></Card.Text>
       </Card.Body>
       <Card.Body>
         <Card.Text>{props.valLoan}</Card.Text>
@@ -14,10 +14,10 @@ export default function CardCars(props) {
       <Card.Body>
         <Button variant="success">SHOW THE EDMUNDS DEAL</Button>
         <Button variant="success">GET DEALER OFFER</Button>
-        <Card.Text>{props.carsCard['dealerName']}</Card.Text>
-        <Card.Text><Card.Link href="#">{props.carsCard['dealerUrl']}</Card.Link></Card.Text>
-        <Card.Text>{props.carsCard['dealerPhone']}</Card.Text>
-        <Card.Text>({props.carsCard['dealerReviews']} reviews)</Card.Text>
+        <Card.Text>{props.dealerName}</Card.Text>
+        <Card.Text><Card.Link href="#">{props.dealerUrl}</Card.Link></Card.Text>
+        <Card.Text>{props.dealerPhone}</Card.Text>
+        <Card.Text>({props.dealerReviews} reviews)</Card.Text>
         <Card.Text>Directions</Card.Text>
       </Card.Body>
     </Card>
