74b586b87c5e76d91e8cb9d3e1ca34cece20b304
nikway-git <victory.adm@yandex.ru>
2020-02-12 18:58:18 +0300

    main code

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,31 @@
-import React, { Component } from "react";
-
+import React, { Suspense, useState } from "react";
+import { Container, Row, Col } from 'react-bootstrap';
 import '../styles/App.css';
+import TabsWithLoanLease from './TabsWithLoanLease/TabsWithLoanLease';
+import CardCars from './CardCars/CardCars';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Stiff React App!</h1>
-            </div>
-        );
-    }
-}
+function App(props) {
+  const priceMSRP="23240";
+  const valLoan="555";
+  const dealerName="Minsk Volvo Center"
+  const dealerUrl="www.volvo-center13.by"
+  const dealerPhone="+375 99 999 99 99"
+  const dealerReviews="78"
+  const [value, setValue] = useState('-');
+  const handleChangeTab = val => {
+    setValue(val);
+  }
+
+    return (    
+      <Suspense fallback="loading">
+        <Container>
+          <Row>
+            <Col sm={8}><TabsWithLoanLease priceMSRP={priceMSRP} handleChangeTab={handleChangeTab} /></Col>
+            <Col sm={4}><CardCars priceMSRP={priceMSRP} valLoan={value} dealerName={dealerName} dealerUrl={dealerUrl} dealerPhone={dealerPhone} dealerReviews={dealerReviews} /></Col>
+          </Row>
+        </Container>
+      </Suspense>
+    );
+  }
 
 export default App;
---
+++ src/components/TabsWithLoanLease/FormLoan/FormLoan.js
@@ -0,0 +1,134 @@
+import React, { useState, useEffect } from 'react';
+import { Form, Col, ToggleButtonGroup,  ToggleButton, InputGroup, FormControl} from 'react-bootstrap';
+
+export default function FormLoan(props) {
+  let [value, setValue] = useState([24]);
+  let [value2, setValue2] = useState([1.05]);
+  //const handleChange = val => setValue(val);
+  const handleChange = val => {
+    setValue(val);
+    runLoan();
+  };
+  const handleChange2 = val => {
+    setValue2(val);
+    runLoan();
+  };
+  const [tradeIn, setTradeIn] = useState("0");
+  const [dawnPayment, setDawnPayment] = useState("0");
+  const [apr, setApr] = useState("0");
+  const [zip, setZip] = useState("-");
+
+  function runLoan () {
+    /*console.log(props.priceMSRP);
+    console.log(tradeIn);
+    console.log(dawnPayment);
+    console.log(value);
+    console.log(apr);
+    console.log((100+Number(apr))/100);
+    console.log(value2);*/
+    let valLoan=Math.round((props.priceMSRP-tradeIn-dawnPayment)/value*((100+Number(apr))/100)*value2);
+    props.handleChangeLoan(valLoan);
+  };
+
+  return (
+    <>
+      <Form>
+        <Form.Row>
+          <Col xs="auto">
+            <Form.Label>Edmunds Suggested Price</Form.Label>
+          </Col>
+          <Col className="d-flex justify-content-end">
+            <Form.Label>${props.priceMSRP}</Form.Label>
+          </Col>
+        </Form.Row>
+        <Form.Row>
+          <Col xs="auto">
+            <Form.Label>Term (Months)</Form.Label>
+          </Col>
+        </Form.Row>
+        <Form.Row>
+          <ToggleButtonGroup name="options" value={value} onChange={handleChange} size="lg" onBlur={runLoan} >
+            <ToggleButton variant="outline-dark" value={12}>12</ToggleButton>
+            <ToggleButton variant="outline-dark" value={24}>24</ToggleButton>
+            <ToggleButton variant="outline-dark" value={36}>36</ToggleButton>
+            <ToggleButton variant="outline-dark" value={48}>48</ToggleButton>
+            <ToggleButton variant="outline-dark" value={72}>72</ToggleButton>
+            <ToggleButton variant="outline-dark" value={84}>84</ToggleButton>
+          </ToggleButtonGroup>
+        </Form.Row>
+        <Form.Row>
+          <Col xs="auto">
+            <Form.Label>Trade-In Value</Form.Label>
+          </Col>
+          <Col xs="2" className="justify-content-end">
+            <InputGroup className="mb-3" >
+              <InputGroup.Prepend>
+                <InputGroup.Text>$</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl value={tradeIn} onChange={(event) => setTradeIn(event.target.value)} onBlur={runLoan} onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  runLoan()
+                }
+              }} />
+            </InputGroup>
+          </Col>
+        </Form.Row>
+        <Form.Row>
+          <Col xs="auto">
+            <Form.Label>Down Payment</Form.Label>
+          </Col>
+          <Col xs="2" className="justify-content-end">
+            <InputGroup className="mb-3" >
+              <InputGroup.Prepend>
+                <InputGroup.Text>$</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl value={dawnPayment} onChange={(event) => setDawnPayment(event.target.value)} onBlur={runLoan} onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  runLoan()
+                }
+              }} />
+            </InputGroup>
+          </Col>
+        </Form.Row>
+        <Form.Row>
+          <Col xs="auto">
+            <Form.Label>Approx. Credit Score</Form.Label>
+          </Col>
+        </Form.Row>
+        <Form.Row>
+          <ToggleButtonGroup name="options2" value={value2} onChange={handleChange2} size="lg" onBlur={runLoan} >
+            <ToggleButton variant="outline-dark" value={0.95}><b>Poor</b>      <br/>639 or less</ToggleButton>
+            <ToggleButton variant="outline-dark" value={1}><b>Fair</b>      <br/>640 - 699</ToggleButton>
+            <ToggleButton variant="outline-dark" value={1.05}><b>Good</b>      <br/>700 - 749</ToggleButton>
+            <ToggleButton variant="outline-dark" value={1.2}><b>Excellent</b> <br/>750 - 850</ToggleButton>
+          </ToggleButtonGroup>
+        </Form.Row>
+        <Form.Row>
+          <Col xs="auto">
+            <Form.Label>Estimated APR</Form.Label>
+          </Col>
+          <Col xs="2" className="justify-content-end">
+            <InputGroup className="mb-3" >
+              <FormControl value={apr} onChange={(event) => setApr(event.target.value)} onBlur={runLoan} onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  runLoan()
+                }
+              }} />
+              <InputGroup.Append>
+                <InputGroup.Text>%</InputGroup.Text>
+              </InputGroup.Append>
+            </InputGroup>
+          </Col>
+        </Form.Row>
+        <Form.Row>
+          <Col xs="auto">
+            <Form.Label>For ZIP Code</Form.Label>
+          </Col>
+          <Col xs="2" className="justify-content-end">
+            <FormControl value={zip} readOnly />
+          </Col>
+        </Form.Row>
+      </Form>
+    </>
+  );
+}
